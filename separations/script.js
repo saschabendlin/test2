a7dc1970e9e9b94872937de28d5e8244b55ec230
sascha bendlin <saschabendlin@gmail.com>
2019-01-16 16:36:30 +0800

    test separate files

--- separations/script.js
+++ separations/script.js
@@ -1,63 +1,5 @@
-<!DOCTYPE html>
-<html>
-<script src="https://cdn.jsdelivr.net/npm/vue"></script>
-<style>
-table {
-  font-family: 'Open Sans', sans-serif;
-  width: 750px;
-  border-collapse: collapse;
-  border: 3px solid #44475C;
-  margin: 10px 10px 0 10px;
-}
 
-table th {
-  text-transform: uppercase;
-  text-align: left;
-  background: #44475C;
-  color: #FFF;
-  cursor: pointer;
-  padding: 8px;
-  min-width: 30px;
-}
-table th:hover {
-        background: #717699;
-      }
-table td {
-  text-align: left;
-  padding: 8px;
-  border-right: 2px solid #7D82A8;
-}
-table td:last-child {
-  border-right: none;
-}
-table tbody tr:nth-child(2n) td {
-  background: #D4D8F9;
-}
-
-</style>
-<body>
-
-<div id="app">
-  <h1>{{ message }}</h1>
-</div>
-
-
-<table id="thirdTable">
-  <thead>
-    <tr>
-      <th v-for="col in columns" v-on:click="sortTable(col)">{{col}}</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="row in rows">
-      <td v-for="col in columns">{{row[col]}}</td>
-    </tr>
-  </tbody>
-</table>
-
-
-
-<script> 
+ 
 
 var thirdTable = new Vue({
   el: '#thirdTable',
@@ -92,10 +34,3 @@ var thirdTable = new Vue({
     }
   }
 });
-
-</script>
-
-
-
-</body>
-</html>
