103441c23a3f1836e7e8f8e6917992791b69ad15
paizah <yusriiza@gmail.com>
2019-01-23 15:03:57 +0800

    perubahan ui sql dan aplikasi

--- DCCM/ui/aplikasi/script2.js
+++ DCCM/ui/aplikasi/script2.js
@@ -5,12 +5,10 @@ var thirdTable1 = new Vue({
     el: '#thirdTable1',
     data: {   
       rows: [
-        { Bil: 1, Dataset: "Chandler Bing", MemberProgram : '305-917-1301', Status: 'IT Manager', Catatan: 'denbhgr', DB2BukanDB2 : 'Baru', NamaPelanDB2 : 'Baru', NamaGrant : 'Baru', Tambah : 'Baru'},
+        { Bil: 1, Dataset: "Chandler Bing", Member_Program : '305-917-1301', Status: 'IT Manager', Catatan: 'denbhgr', DB2_Bukan_DB2 : 'Baru', Nama_Pelan_DB2 : 'Baru', Nama_Grant : 'Baru'},
         { Bil: 2, Dataset: "nike", MemberProgram: '210-684-8953', Status: 'Paleontologist',  },
-        { Bil: 3, Dataset: "sal", phone: '765-338-0312', profession: 'Waitress'},
-        { Bil: 4, Dataset: "Monica", phone: '714-541-3336', profession: 'Head Chef' },
-        { Bil: 5, Dataset: "Joey ", phone: '972-297-6037', profession: 'Actor' },
-        { Bil: 6, Dataset: " Buffay", phone: '760-318-8376', profession: 'Masseuse' }
+        { Bil: 3, Dataset: "sal", phone: '765-338-0312', profession: 'Waitress'}
+        
       ]
     
           
